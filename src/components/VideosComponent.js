6d0ff926a62d693dadafef3a3b3bc7bc88563630
Voren Elver I. Tejuco <vitejuco@mycitihomes.local>
2024-06-13 12:00:01 +0800

    added video player controls

--- src/components/VideosComponent.js
+++ src/components/VideosComponent.js
@@ -1,7 +1,9 @@
 import ReactPlayer from 'react-player';
 import Spinner from './Spinner.js';
 import '../assets/css/partial-css/video.css';
-
+import playBtn from '../assets/images/play.png';
+import soundz from '../assets/images/soundz.png';
+import fullscreen from '../assets/images/fullscreen.png';
 
 
 const VideosComponent = ({videos, isLoading}) => {
@@ -20,9 +22,28 @@ const VideosComponent = ({videos, isLoading}) => {
                                       url={video.src} 
                                       width="100%"
                                       height="100%"
-                                      controls
+                                      controls={false}
                                       className="react-player"
                                       />
+
+                                      <div className="controls-wrapper">
+                                        <h2 className="controls-title">{video.title}</h2>
+                                        <div className="controls-middle">
+                                             <img src={playBtn} className="playBtnMid" />
+                                        </div>
+                                        <div className="controls-bottom">
+                                             <input type="range" />
+                                             <div className="controls-bottom2">
+                                                  <img src={playBtn} className="bottomBtns" />
+                                                  <div>
+                                                       <img src={soundz} className="bottomBtns" />
+                                                       <input type="range" />
+                                                  </div> 
+                                                  <img src={fullscreen} className="bottomBtns" />
+                                             </div>
+                                             
+                                        </div>
+                                      </div>
                                    </div>
                                    <h1 className="video-title">{video.title}</h1>
                               </div>
